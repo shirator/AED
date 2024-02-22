56d708cae50cd9000a617a35aefc6ac2284803f8
Andrezinho <andrekaled9@gmail.com>
2024-02-22 19:46:10 -0400

    exercicio 01

---
+++ 01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+  int M[10];
+
+  for (int i = 0; i < M; i++)
+  {
+    printf("Insira o %dº valor: \n", i + 1);
+    scanf("%d", &);
+  }
+
+  for (int j = 0; j < M; j++)
+  {
+    printf("%d ", M[j])
+  }
+
+  return 0;
+}
